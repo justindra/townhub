821a9f767692c4debb9a7b403d29c3c2a73394bb
Justin Rahardjo <justindra91@gmail.com>
2020-09-18 10:22:58 -0700

    test: setup some example tests

--- infrastructure/cdk/lib/index.ts
+++ infrastructure/cdk/lib/index.ts
@@ -1,9 +1,6 @@
-import MyStack from './MyStack';
 import { App } from '@serverless-stack/resources';
 import ApiGatewayStack from './api-gateway';
 
 export default function main(app: App): void {
-  new MyStack(app, 'my-stack');
-
   new ApiGatewayStack(app, 'ApiGatewayStack', undefined, 'townhub.ca');
 }
