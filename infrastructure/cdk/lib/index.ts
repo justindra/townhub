275e87a3935e0423406b6896d8464e0b77d1e556
Justin Rahardjo <justindra91@gmail.com>
2020-09-23 20:42:25 -0700

    feat: added an example of a service written using SLS

--- infrastructure/cdk/lib/index.ts
+++ infrastructure/cdk/lib/index.ts
@@ -3,11 +3,13 @@ import ApiGatewayStack from './api-gateway';
 import StaticSiteStack from './static-site';
 
 export default function main(app: App): void {
+  const rootDomainName = 'townhub.ca';
+
   new ApiGatewayStack(app, 'ApiGatewayStack', {
-    rootDomainName: 'townhub.ca',
+    rootDomainName,
   });
 
   new StaticSiteStack(app, 'StaticHomePageStack', {
-    rootDomainName: 'townhub.ca',
+    rootDomainName,
   });
 }
