bc4d13ab187c1d8494d9416e6a59cabdf6b8f3f1
Justin Rahardjo <justindra91@gmail.com>
2020-10-12 21:36:39 -0700

    updated shuttle and test

--- backend/shuttle/src/get-daily-data.ts
+++ backend/shuttle/src/get-daily-data.ts
@@ -1,14 +1,13 @@
-import { getDailyData } from '@townhub-libs/core/features/shuttle';
-import { DailyData } from '@townhub-libs/core/features/shuttle/interfaces';
-import { ApiGatewayWrapper } from '@townhub-libs/core/wrappers';
+import { ApiGatewayWrapper, getDailyData, DailyData } from '@townhub-libs/core';
 
 /**
  * Gets the daily data for a town's shuttle service
  */
-export const main = ApiGatewayWrapper<DailyData, { timestamp: number }>(
+export const main = ApiGatewayWrapper<DailyData, { timestamp: string }>(
   async ({ townId, pathParameters: { timestamp } }) => {
+    const timestampNumber = parseInt(timestamp);
     // TODO: Set the timezone to be the town's timezone rather than default
-    const dailyData = await getDailyData(townId, timestamp);
+    const dailyData = await getDailyData(townId, timestampNumber);
     return {
       message: 'Found some daily data',
       data: dailyData,
