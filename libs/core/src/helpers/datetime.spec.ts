cf58bd03d828d35f907d2e89243128e430a744f8
Justin Rahardjo <justindra91@gmail.com>
2020-10-13 20:39:53 -0700

    fix: update tests

--- libs/core/src/helpers/datetime.spec.ts
+++ libs/core/src/helpers/datetime.spec.ts
@@ -4,21 +4,26 @@ describe('datetime helpers', () => {
   describe('getDayDateRange', () => {
     it('should return the correct time when in the default timezone', () => {
       const timestamp = 1602369473215;
-      const { startOfDay, middleOfDay, endOfDay } = getDayDateRange(timestamp);
-      expect(startOfDay).toEqual(1602313200000);
-      expect(middleOfDay).toEqual(1602356400000);
-      expect(endOfDay).toEqual(1602399599999);
+      const {
+        startOfDayValue,
+        middleOfDayValue,
+        endOfDayValue,
+      } = getDayDateRange(timestamp);
+      expect(startOfDayValue).toEqual(1602313200000);
+      expect(middleOfDayValue).toEqual(1602356400000);
+      expect(endOfDayValue).toEqual(1602399599999);
     });
 
     it('should return the correct time when in a different timezone', () => {
       const timestamp = 1602369473215;
-      const { startOfDay, middleOfDay, endOfDay } = getDayDateRange(
-        timestamp,
-        'Australia/Brisbane'
-      );
-      expect(startOfDay).toEqual(1602338400000);
-      expect(middleOfDay).toEqual(1602381600000);
-      expect(endOfDay).toEqual(1602424799999);
+      const {
+        startOfDayValue,
+        middleOfDayValue,
+        endOfDayValue,
+      } = getDayDateRange(timestamp, 'Australia/Brisbane');
+      expect(startOfDayValue).toEqual(1602338400000);
+      expect(middleOfDayValue).toEqual(1602381600000);
+      expect(endOfDayValue).toEqual(1602424799999);
     });
   });
 });
