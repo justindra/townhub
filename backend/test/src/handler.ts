19d5fcc60ef883ce3ea87f73237949b6f2256ffa
Justin Rahardjo <justindra91@gmail.com>
2020-10-13 21:13:16 -0700

    fix: remove test variables

--- backend/test/src/handler.ts
+++ backend/test/src/handler.ts
@@ -1,27 +1,11 @@
-import {
-  HELLO_CAMEL,
-  HELLO_SENTENCE,
-  HELLOCLASS,
-  TEST,
-} from '@townhub-libs/core';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
-class TestClass {
-  magic: TEST;
-  constructor() {
-    this.magic = `magic = ${HELLO_CAMEL}`;
-  }
-}
-
 export const hello: APIGatewayProxyHandler = async (event) => {
-  const a = new TestClass();
-  const b = new HELLOCLASS();
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        test: `${HELLO_CAMEL}, ${HELLO_SENTENCE}, ${a.magic}, ${b.getSome()}`,
         input: event,
       },
       null,
