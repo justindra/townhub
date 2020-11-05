84b4f142be1556739e28a75d95f147965ceefd39
Justin Rahardjo <justindra91@gmail.com>
2020-11-04 21:20:14 -0800

    chore: add redux-dev-tools for dev

--- apps/web/src/sdk/index.ts
+++ apps/web/src/sdk/index.ts
@@ -1,4 +1,4 @@
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { offline } from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 
@@ -6,4 +6,13 @@ const reducer = () => {
   return {};
 };
 
-export const store = createStore(reducer, offline(offlineConfig));
+// Allow redux-dev-tools in development
+const composeEnhancers =
+  process.env.NODE_ENV === 'development'
+    ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+    : compose;
+
+export const store = createStore(
+  reducer,
+  composeEnhancers(offline(offlineConfig))
+);
