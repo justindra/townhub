75447f77672c90937556e0a4bd2f32c8739661cc
Justin Rahardjo <justindra91@gmail.com>
2020-12-06 11:21:29 -0800

    fix: missing braces

--- apps/web/src/components/loading.tsx
+++ apps/web/src/components/loading.tsx
@@ -12,7 +12,7 @@ const useLoadingPageStyles = makeStyles((theme) => ({
     position: 'absolute',
     top: '50%',
     left: '50%',
-    transform: 'translate(-50%, -50%',
+    transform: 'translate(-50%, -50%)',
   },
 }));
 
