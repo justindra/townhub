edbb8812033b6efb98d82f7b37b4d7324ccd808e
Justin Rahardjo <justindra91@gmail.com>
2021-07-02 16:58:23 -0700

    refactor: replace SSL with a built-in construct (#34)
    * feat: replace the old way of doing SSL with one with AWS-sdk
    
    * chore: remove ssl infrastructure
    
    * uncomment code

--- infrastructure/ssl/lib/index.ssl.ts
+++
@@ -1,8 +0,0 @@
-import { App } from '@serverless-stack/resources';
-import { SslCertificateStack } from './ssl-certificate';
-
-export default function main(app: App): void {
-  new SslCertificateStack(app, 'SSLGlobalCertificateSource', {
-    rootDomainName: 'townhub.ca',
-  });
-}
