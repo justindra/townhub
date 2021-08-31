f9b279687ec089350c6d14a83d26c737b35b8d39
Justin Rahardjo <justindra91@gmail.com>
2021-08-30 22:28:19 -0700

    chore: remove modules from towns directly

--- libs/towns/src/interfaces.ts
+++ libs/towns/src/interfaces.ts
@@ -1,11 +1,6 @@
 import { BaseEntity } from '@townhub-libs/core';
 import { TOWNS_DATABASE } from './constants';
 
-interface ModuleSetting {
-  /** The name of the module */
-  name: 'shuttles';
-}
-
 export interface Town extends BaseEntity<typeof TOWNS_DATABASE.ENTITY_TYPE> {
   /** A human-readable ID, can be used as a slug too */
   hid: string;
@@ -13,6 +8,4 @@ export interface Town extends BaseEntity<typeof TOWNS_DATABASE.ENTITY_TYPE> {
   name: string;
   /** The timezone the town is in, we might use this for defaults */
   timezone: string;
-  /** The modules that has been assigned to this town and settings for it */
-  modules: ModuleSetting[];
 }
