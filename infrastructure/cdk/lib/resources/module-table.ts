3ca30eea85b6cb180cfeef70c3eb0737d30ef040
Justin Rahardjo <justindra91@gmail.com>
2021-08-30 20:30:11 -0700

    feat: set GSI to be optional

--- infrastructure/cdk/lib/resources/module-table.ts
+++ infrastructure/cdk/lib/resources/module-table.ts
@@ -3,6 +3,10 @@ import { AttributeType } from '@aws-cdk/aws-dynamodb';
 import { DEFAULT_SECONDARY_INDEXES } from '@townhub-libs/constants';
 import { TownhubTable, TownhubTableProps } from './table';
 
+export interface TownhubModuleTableProps extends TownhubTableProps {
+  addGSI?: boolean;
+}
+
 /**
  * A Table used in Townhub for all entities that belong to a specific module.
  * It adds default secondary indexes so that we can easily search by the townId
@@ -13,25 +17,31 @@ import { TownhubTable, TownhubTableProps } from './table';
  * @output TableStreamArn (optional)
  */
 export class TownhubModuleTable extends TownhubTable {
-  constructor(scope: Construct, id: string, props: TownhubTableProps = {}) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: TownhubModuleTableProps = {}
+  ) {
     super(scope, id, props);
 
-    // Add the secondary index so we can list by the town id
-    this.ddbTable.addGlobalSecondaryIndex({
-      indexName: DEFAULT_SECONDARY_INDEXES.BY_TOWN.name,
-      partitionKey: {
-        name: DEFAULT_SECONDARY_INDEXES.BY_TOWN.key,
-        type: AttributeType.STRING,
-      },
-    });
+    if (props.addGSI) {
+      // Add the secondary index so we can list by the town id
+      this.ddbTable.addGlobalSecondaryIndex({
+        indexName: DEFAULT_SECONDARY_INDEXES.BY_TOWN.name,
+        partitionKey: {
+          name: DEFAULT_SECONDARY_INDEXES.BY_TOWN.key,
+          type: AttributeType.STRING,
+        },
+      });
 
-    // Add the secondary index so we can list by the module id
-    this.ddbTable.addGlobalSecondaryIndex({
-      indexName: DEFAULT_SECONDARY_INDEXES.BY_MODULE.name,
-      partitionKey: {
-        name: DEFAULT_SECONDARY_INDEXES.BY_MODULE.key,
-        type: AttributeType.STRING,
-      },
-    });
+      // Add the secondary index so we can list by the module id
+      this.ddbTable.addGlobalSecondaryIndex({
+        indexName: DEFAULT_SECONDARY_INDEXES.BY_MODULE.name,
+        partitionKey: {
+          name: DEFAULT_SECONDARY_INDEXES.BY_MODULE.key,
+          type: AttributeType.STRING,
+        },
+      });
+    }
   }
 }
