80443985ee177c24e32e38f25d5e98af96695395
Justin Rahardjo <justindra91@gmail.com>
2021-09-14 08:28:26 -0700

    feat: play with routes

--- apps/admin/src/my-app.ts
+++ apps/admin/src/my-app.ts
@@ -1,1 +1,25 @@
-export class MyApp {}
+import { route } from 'aurelia';
+
+@route({
+  routes: [
+    {
+      id: 'home',
+      path: ['', 'welcome-page'],
+      component: import('./welcome-page'),
+      title: 'Home',
+    },
+    {
+      id: 'about',
+      path: 'about-page',
+      component: import('./about-page'),
+      title: 'About',
+    },
+    // { path: 'login', component: import('./auth'), title: 'Sign in' },
+    // { path: 'register', component: import('./auth'), title: 'Sign up' }
+  ],
+})
+export class MyApp {
+  configureRouter(te: any) {
+    console.log(te);
+  }
+}
