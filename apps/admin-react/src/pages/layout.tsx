bc07192b387830cd47e0908a77d24c163878e71f
Justin Rahardjo <justindra91@gmail.com>
2021-09-18 18:04:04 -0700

    feat(admin): added TailwindCSS as a styling library

--- apps/admin-react/src/pages/layout.tsx
+++ apps/admin-react/src/pages/layout.tsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export const PageLayout: FC = ({ children }) => {
   return (
-    <div>
+    <div className='h-screen bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50'>
       <NavLink to='/' exact activeStyle={{ background: 'red' }}>
         Home
       </NavLink>
