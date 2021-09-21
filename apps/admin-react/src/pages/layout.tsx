2195c4a44e894fef2b560920ad7f9c71515cdf25
Justin Rahardjo <justindra91@gmail.com>
2021-09-20 22:25:02 -0700

    style(admin): updated styling of sidebar and added profile picture at top right (#38)
    * feat: added more styling
    
    * feat: updated styling with colors
    
    * feat: added the user profile

--- apps/admin-react/src/pages/layout.tsx
+++ apps/admin-react/src/pages/layout.tsx
@@ -1,31 +1,56 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import { FC } from 'react';
 import { NavLink, NavLinkProps } from 'react-router-dom';
 
 const NavItem: FC<NavLinkProps> = ({ isActive, ...props }) => {
   return (
     <NavLink
-      className='w-full flex items-center p-4 transition-colors duration-200 justify-start'
-      activeClassName='text-blue-400 font-bold bg-gradient-to-r from-white via-transparent dark:from-gray-600 border-l-4 border-blue-600'
+      className='max-w-full flex rounded-xl items-center py-2 px-4 my-1 transition-colors duration-300 justify-start text-gray-300 hover:bg-blue-400 hover:bg-opacity-25'
+      activeClassName='font-bold bg-blue-600 hover:bg-blue-700 hover:bg-opacity-100'
       {...props}
     />
   );
 };
 
+const UserProfile: FC = () => {
+  const { user } = useAuth0();
+  return (
+    <img
+      src={user.picture}
+      alt={user.name}
+      className='object-cover rounded-full h-10 w-10 '
+    />
+  );
+};
+
 export const PageLayout: FC = ({ children }) => {
+  const { logout } = useAuth0();
   return (
     <div className='h-screen bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-100 flex'>
-      <div className='h-screen hidden lg:block relative w-48'>
-        <div className='flex items-center justify-center p-4 text-lg font-bold uppercase'>
+      <div className='h-screen hidden lg:flex relative w-48 flex-col'>
+        <div className='flex items-center justify-center p-4 py-5 text-lg font-bold uppercase'>
           Townhub
         </div>
-        <nav className='mt-2'>
+        <nav className='mt-2 mx-4 flex-1'>
           <NavItem to='/' exact>
             Home
           </NavItem>
           <NavItem to='/profile'>Profile</NavItem>
         </nav>
+        <nav className='my-2 mx-4'>
+          <button
+            className='w-full text-left rounded-xl items-center py-2 px-4 my-1 transition-colors duration-300 justify-start text-red-500 hover:bg-red-500 hover:bg-opacity-25'
+            onClick={() => logout({ returnTo: window.location.origin })}>
+            Logout
+          </button>
+        </nav>
       </div>
-      <div>{children}</div>
+      <div className='flex-1'>
+        <div className='w-full p-4 flex items-center justify-end'>
+          <UserProfile />
+        </div>
+        <div className='p-4'>{children}</div>
+      </div>
     </div>
   );
 };
